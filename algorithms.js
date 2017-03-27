aec70b7e7854128cf74ea6f42a521d6ad1795278
chiasms <chasms@chas.ms>
2017-03-27 10:23:31 -0400

    added insertion sort and recursive palindrome checker

--- algorithms.js
+++ algorithms.js
@@ -19,6 +19,92 @@ var doSearch = function(array, targetValue) {
     return -1;
 };
 
+var doLinearSearch = function(array, targetValue) {
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] === targetValue) {
+			return i;
+		}
+	}
+}
+
+var selectionSort = function(array) {							//	O(n^2)
+    for(var i = 0; i < array.length; i++) {
+        swap(array, i, indexOfMinimum(array, i)); //	O(n)
+    }
+};
+
+var insertionSort = function(array) {
+	for (var i = 1; i < array.length; i++) {
+		insert(array, i - 1, array[i]);
+	}
+}
+
+var swap = function(array, firstIndex, secondIndex) {
+	var temp = array[firstIndex];
+	array[firstIndex] = array[secondIndex];
+	array[secondIndex] = temp;
+};
+
+var indexOfMinimum = function(array, startIndex) {
+    // Set initial values for minValue and minIndex,
+    // based on the leftmost entry in the subarray:
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
+
+    // Loop over items starting with startIndex,
+    // updating minValue and minIndex as needed:
+    for(var i = minIndex + 1; i < array.length; i++) {
+        if (array[i] < minValue) {
+            minValue = array[i];
+            minIndex = i;
+        }
+    }
+    return minIndex;
+};
+
+var insert = function(array, rightIndex, value) {
+	var j
+	for(j = rightIndex; j >= 0 && array[j] > value; j--) {
+		array[j + 1] = array[j];
+	}
+	array[j + 1] = value;
+}
+
+// Returns the first character of the string str
+var firstCharacter = function(str) {
+    return str.slice(0, 1);
+};
+
+// Returns the last character of a string str
+var lastCharacter = function(str) {
+    return str.slice(-1);
+};
+
+// Returns the string that results from removing the first
+//  and last characters from str
+var middleCharacters = function(str) {
+    return str.slice(1, -1);
+};
+
+var isPalindrome = function(str) {
+    // base case #1
+    if (str.length <= 1) {
+        return true;
+    } else if (firstCharacter(str) !== lastCharacter(str)) {
+        return false;
+    }
+    else {
+        return isPalindrome(middleCharacters(str));
+    }
+    // base case #2
+    // recursive case
+};
+
+var checkPalindrome = function(str) {
+    println("Is this word a palindrome? " + str);
+    println(isPalindrome(str));
+};
+
 var println = function(x) {
   console.log(x)
 }
@@ -26,5 +112,20 @@ var println = function(x) {
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
 		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
+var array = [3, 5, 7, 11, 13, 2, 9, 6];
+
+insert(array, 4, 2);
+println("Array after inserting 2:  " + array);
+
+insert(array, 5, 9);
+println("Array after inserting 9:  " + array);
+
+insert(array, 6, 6);
+println("Array after inserting 6:  " + array);
+
 var result = doSearch(primes, 73);
 println("Found prime at index " + result);
+
+checkPalindrome("a");
+checkPalindrome("motor");
+checkPalindrome("rotor");
--- algorithms.rb
+++ algorithms.rb
@@ -1,29 +1,98 @@
-class Algorithms
 
-  def binary_search(array:, value:, min: 0, max: nil)
-    max = array.size - 1 unless max
-    mid = avg(min, max)
-    if value < array[mid]
-      return binary_search(array: array, value: value, min: min, max: mid-1)
-    elsif value > array[mid]
-      return binary_search(array: array, value: value, min: mid + 1, max: max)
-    else
-      return mid
-    end
+def binary_search(array:, value:, min: 0, max: nil)
+  max = array.size - 1 unless max
+  mid = avg(min, max)
+  if value < array[mid]
+    return binary_search(array: array, value: value, min: min, max: mid-1)
+  elsif value > array[mid]
+    return binary_search(array: array, value: value, min: mid + 1, max: max)
+  else
+    return mid
   end
-
-  private
-
-    def avg(*nums)
-        sum(nums)/nums.size #size is constant
-    end
-
-    def sum(array) #sum iterates, so it is linear
-      array.inject(0, :+)
-    end
-
 end
 
-array = [1,2,3,4,5,6,7,8,9]
+def linear_search(array:, value:)
+  array.each_with_index do |item, index|
+    return index if item == value
+  end
+  nil
+end
+
+def selection_sort(array:)
+  array.each_with_index do |value, index|
+    swap(array, index, index_of_minimum(array, i))
+  end
+end
+
+def insertion_sort(array:)
+  i = 1
+  (array.length - 1).times do
+    insert(array: array, right_index: i - 1, value: array[i])
+    i+=1
+  end
+  array
+end
+
+def factorial(n:)
+  if n == 0
+    return 1
+  else
+    return n * factorial(n: n-1)
+  end
+end
+
+def insert(array:, right_index:, value:)
+  i = right_index
+  while i >= 0 && array[i] > value
+    array[i + 1] = array[i]
+    i-=1
+  end
+  array[i + 1] = value
+  array
+end
+
+def swap(array:, index1:, index2:)
+  temp = array[index1]
+  array[index1] = array[index2]
+  array[index2] = temp
+end
+
+def avg(*nums)
+    sum(nums)/nums.size #size is constant
+end
+
+def sum(array) #sum iterates, so it is linear
+  array.inject(0, :+)
+end
+
+def index_of_minimum(array:, start_index:)
+  min = array[startIndex]
+  min_index = start_index
+  array.each_with_index do |value, index|
+    if value < min
+      min = value
+      min_index = index
+    end
+  end
+  min_index
+end
+
+def test_insert(array:)
+  puts "Testing insert for unsorted array #{array}"
+  i = 1
+  (array.length-1).times do
+    print "\nInsert #{array[i]} (array[#{i}]) into #{array}:\n#{insert(array: array, right_index: i-1, value: array[i])}\n"
+    i+=1
+  end
+  puts "\nArray sorted! #{array}"
+end
+
+array = [4,5,1,7,2,3,9,6,8]
+sorted_array = [1,2,3,4,5,6,7,8,9]
 value = 6
-puts binary_search(array: array, value: value)
+puts "\nBinary Search for #{value} in #{sorted_array}:\narray[#{binary_search(array: sorted_array, value: value)}] = #{value}\n"
+puts "\nLinear Search for #{value} in #{sorted_array}:\narray[#{linear_search(array: sorted_array, value: value)}] = #{value}\n"
+puts "\nFactorial #{value}:\n#{factorial(n: value)}\n\n"
+test_insert(array: array)
+array = [4,5,1,7,2,3,9,6,8]
+puts "\nInsertion Sort for #{array}:\n#{insertion_sort(array: array)}\n"
